0c52e6c38aabe4e23e60874809c41e2a313d720d
Akiya <akiya.pisces@gmail.com>
2019-04-13 21:19:16 +0900

    :bug: 実行時のURLが通らないのでクラス構造修正

--- src/main/java/merci/api/app/resource/news/NewsListGetResource.java
+++ src/main/java/merci/api/app/resource/news/NewsListGetResource.java
@@ -1,9 +1,11 @@
 package merci.api.app.resource.news;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import merci.api.app.resource.AbstractResource;
 import merci.api.common.util.JsonConverter;
 import merci.api.domain.bean.news.NewsListBean;
 import merci.api.domain.service.news.NewsListGetService;
@@ -11,7 +13,8 @@ import merci.api.domain.service.news.NewsListGetService;
 /**
  * ニュースリスト取得API
  */
-public class NewsListGetResource extends AbstractNewsResource {
+@Path("/news")
+public class NewsListGetResource extends AbstractResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
